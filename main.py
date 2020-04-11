9f07acc913b8989504f2b446ed343a01bce70a7d
brandonyuh <brandonyuh@gmail.com>
2020-04-11 14:32:02 -0700

    Added tkinter Added file and quit menu

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import tkinter as tk
+root = tk.Tk()
+
 def main():
-    print("Hello World!")
+    menu_setup(root)
+    root.mainloop()
+
+def menu_setup(root):
+    menubar = tk.Menu(root)
+    filemenu = tk.Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Exit", command=quit)
+    menubar.add_cascade(label="File", menu=filemenu)
+    root.config(menu=menubar)
+
+def quit():
+    root.quit()
 
 if __name__ == "__main__":
     main()
